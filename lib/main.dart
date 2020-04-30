7fd250df0cc197b6c6a3dc7ae6eb3226eeb308eb
Ergun Polat <ergunpolat@msn.com>
2020-04-30 13:03:33 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19 Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(child: Text("Hello")),
+      ),
+    );
+  }
+}
